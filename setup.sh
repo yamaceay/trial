e29e3a2939a3ad9ac2290c7feb7c325ad1600890
unknown <yamacerenay2001@gmail.com>
2020-10-15 18:11:30 +0300

    simple app v6

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 mkdir -p ~/.streamlit/
+# shellcheck disable=SC2028
 echo "\
 [general]\n\
 email = \"yamacerenay2001@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+# shellcheck disable=SC2028
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-port = $PORT\n\"
+port = $PORT\n\
+"
